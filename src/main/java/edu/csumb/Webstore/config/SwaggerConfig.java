1e7dff7ef46bea53fc9ba98b75b17b4ed2c29ae1
Elijah Baird <elijah_baird84@yahoo.com>
2019-10-17 19:14:46 -0700

    Starting sprint 3

--- src/main/java/edu/csumb/Webstore/config/SwaggerConfig.java
+++ src/main/java/edu/csumb/Webstore/config/SwaggerConfig.java
@@ -15,13 +15,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class Swagger {                                    
+public class SwaggerConfig {                                    
     @Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
           .build();                                           
     }
 }
